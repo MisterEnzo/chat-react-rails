74f01e5bd726ca73552c0d81f1fec58eff3d2349
enz <enzo_naguiat@yahoo.com>
2019-07-04 21:23:30 +0900

    modify session and signup buttons to show when user isn't logged in, or to change to logout if the user is signed-in. Render the Chat App div that the chat-app will be mounted to

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def login_helper
+    if !current_user
+      (link_to "Sign-up", new_user_registration_path, class: "item") + " ".html_safe +
+      (link_to "Login", new_user_session_path, class: "item")
+    else
+      (link_to "Logout", destroy_user_session_path, method: :delete, class: "item" )
+    end
+  end
 end
