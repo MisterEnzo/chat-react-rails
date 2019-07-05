b006094480f9539b959d5e51ffa4b6f6895aa55a
enz <enzo_naguiat@yahoo.com>
2019-07-05 23:22:13 +0900

    setup database and models for the following: Users, Channels, Messages. Also setup their associations via the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # model attributes
+  has_many :messages, dependent: :destroy
 end
