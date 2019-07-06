7da81cf304a843465464c758d1b4a01bf378a636
enz <enzo_naguiat@yahoo.com>
2019-07-06 23:34:54 +0900

    made a seed file for sample channels, users and messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+puts "Creating channels..."
+Channel.create(name: "General")
+Channel.create(name: "Paris")
+Channel.create(name: "React")
+
+puts "Creating users and messages..."
+3.times do |num|
+  User.create(name: "User#{num + 1}", email: "User#{num}@test.com", password: 123123)
+  User.find_by(name: "User#{num + 1}").messages.create(content: "Hello world!", channel_id: 1)
+end
+
+puts "done seeding."
