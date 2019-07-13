c77d40eb8b8417b1f06b9229884cb9241ab94375
enz <enzo_naguiat@yahoo.com>
2019-07-13 22:31:03 +0900

    refactor fetchMessages action to retrieve messages from the rails api

--- app/javascript/chat_app/actions/index.js
+++ app/javascript/chat_app/actions/index.js
@@ -1,20 +1,12 @@
 import * as types from './action_types';
 
 export function fetchMessages(channel) {
-  // return { type: types.FETCH_MESSAGES, messages: [
-  //   {
-  //     "author":"anonymous92",
-  //     "content":"payload",
-  //     "created_at":"2017-09-26T23:03:16.365Z"
-  //   }
-  // ]}
-  // ---
-  return fetch(`https://wagon-chat.herokuapp.com/${channel}/messages`)
+  return fetch(`/api/v1/channels/general/messages`)
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    return { type: types.FETCH_MESSAGES, messages: data.messages}
+    return { type: types.FETCH_MESSAGES, messages: data}
   })
 }
 
--- app/javascript/chat_app/reducers/initial_state.jsx
+++ app/javascript/chat_app/reducers/initial_state.jsx
@@ -1,18 +1,6 @@
 export default {
   channels: ['general', 'react', 'paris'],
-  messages: [
-    {
-      "author":"anonymous92",
-      "content":"Hello world!",
-      "created_at":"2017-09-26T23:03:16.365Z"
-    },
-    {
-      "author":"anonymous77",
-      "content":"My name is anonymous77",
-      "created_at":"2017-09-26T23:03:21.194Z"
-    }
-  ],
+  messages: [],
   selectedChannel: 'general',
-  currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`
-  // currentUser: "anon"
+  currentUser: `anonymous${Math.floor(10 + (Math.random() * 90))}`
 }
