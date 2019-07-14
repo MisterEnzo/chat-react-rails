73c05a05d3ebf8440338320b236ad0db61934fcb
enz <enzo_naguiat@yahoo.com>
2019-07-14 21:23:05 +0900

    delete index_old file. add react-router and create a channel component

--- app/javascript/chat_app/index.jsx
+++ app/javascript/chat_app/index.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+
 import promiseMiddleware from 'redux-promise';
 import { logger } from 'redux-logger';
 
 // internal modules
 import App from './components/app';
+import Channel from './components/channel';
 
 // reducer
 import rootReducer from './reducers/index';
@@ -17,7 +20,10 @@ import rootReducer from './reducers/index';
 
 ReactDOM.render(
   <Provider store={createStore(rootReducer, {}, applyMiddleware(promiseMiddleware, logger))}>
-    <App />
+    <Router history={history}>
+      <Route path="/" exact component={App} />
+      <Route path="/channels/:channel" exact component={Channel} />
+    </Router>
   </Provider>,
   document.getElementById('chat-app')
 )
