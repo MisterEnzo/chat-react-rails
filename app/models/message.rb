9df8374d04f4c4ef1fc02fdffa95b3ac302c8df5
enz <enzo_naguiat@yahoo.com>
2019-08-17 16:35:12 +0900

    add name to name to permitted parameters in devise for user model. modify devise views to have name field when registering new users.  make action cable send messages with relative timestamps.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,15 +1,19 @@
 class Message < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
   belongs_to :user
   belongs_to :channel
 
   after_create :broadcast_message
+
+  # this is how the messages will be sent by action cable
 
   def as_json(options ={})
     {
       id: id,
       author: self.user.name,
       content: content,
-      created_at: time_ago_in_words(`#{created_at}`),
+      created_at: time_ago_in_words(self.created_at),
       channel: self.channel.name
     }
   end
